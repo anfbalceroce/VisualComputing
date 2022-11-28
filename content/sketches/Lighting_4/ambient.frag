631fbee8f8e5acf062aef38b3ef7766d2933e7c3
anfbalceroce <anfbalceroce@unal.edu.co>
2022-11-28 03:02:36 -0500

    multiple lights

--- content/sketches/Lighting_4/ambient.frag
+++ content/sketches/Lighting_4/ambient.frag
@@ -2,23 +2,30 @@ precision mediump float;
 
 uniform float ambient;
 uniform vec4 uMaterialColor;
-uniform vec4 lightColor;
+uniform vec4 ambientColor;
+uniform float lightsColors[12];
 uniform float phongExp;
 uniform float attenuationRate;
 // uLightPosition is given in eye space
-uniform vec3 uLightPosition;
+uniform int lightCount;
+uniform float uLightPosition[12];
 // both, normal3 and position4 are given in eye space as well
 varying vec3 normal3;
 varying vec4 position4;
 
 void main() {
-  vec3 direction3 = uLightPosition - position4.xyz;
-  // solve the diffuse light equation discarding negative values
-  // see: https://thebookofshaders.com/glossary/?search=max
-  // see: https://thebookofshaders.com/glossary/?search=dot
-  float d = length( direction3 );
-  float attenuation =  1.0 / (1.0 + attenuationRate * d);
-  float diffuse = max(0.0, dot(normalize(direction3), normalize(normal3)));
-  float specular = pow(max(0.0, dot(normalize(reflect(-direction3, normal3)), normalize(-vec3(position4)))), phongExp);
-  gl_FragColor = attenuation * (ambient + diffuse + specular) * uMaterialColor * lightColor;
+
+  vec4 sum = vec4(0.0, 0.0, 0.0, 0.0);
+  for (int i = 0; i < 12; i++)
+  {
+    if (i == lightCount) break;
+    vec3 direction3 = vec3(uLightPosition[i*3], uLightPosition[i*3+1], uLightPosition[i*3+2]) - position4.xyz;
+    float d = length( direction3 );
+    float attenuation =  1.0 / (1.0 + attenuationRate * d);
+    float diffuse = max(0.0, dot(normalize(direction3), normalize(normal3)));
+    float specular = pow(max(0.0, dot(normalize(reflect(-direction3, normal3)), normalize(-vec3(position4)))), phongExp);
+    vec4 color = vec4(lightsColors[i*4], lightsColors[i*4+1], lightsColors[i*4+2], lightsColors[i*4+3]);
+    sum += attenuation * (diffuse + specular) * color;
+  }    
+  gl_FragColor =  ((ambient * ambientColor) + sum) * uMaterialColor;
 }
--- content/sketches/Lighting_4/sketch.js
+++ content/sketches/Lighting_4/sketch.js
@@ -4,10 +4,14 @@ let itemsPlane;
 let myShader;
 let ambient;
 let ambientSlider;
-let focos;
 let selectPos;
 let phongExpSlider;
 let attenuationRate;
+let lightCount;
+let lights;
+let colorPicker;
+let colors;
+let lightsColors;
 
 function preload() {
   myShader = readShader('/VisualComputing/sketches/Lighting_4/ambient.frag', { varyings: Tree.normal3 | Tree.position4 });
@@ -46,8 +50,6 @@ function setup() {
     });
   }
   
-  focos = {};
-  
   itemsPlane = createSlider(1, models.length, int(models.length / 3), 1);
   itemsPlane.position(10, 30);
   
@@ -56,18 +58,40 @@ function setup() {
   ambientSlider.input(() => { myShader.setUniform('ambient', ambientSlider.value()) });
 
   selectPos = createSelect();
-  selectPos.position(40, 80);
-  selectPos.option('Centro');
-  selectPos.option('Izquierda');
-  selectPos.option('Derecha');
-  selectPos.selected('Centro');
+  selectPos.position(20, 80);
+  selectPos.option(1);
+  selectPos.option(2);
+  selectPos.option(3);
+  selectPos.selected(1);
+  selectPos.input(() => { 
+  let count = selectPos.value() === 1 ? 1 : selectPos.value() === 2 ? 2 : 3;
+  myShader.setUniform('lightCount', count) });
+
+  colors = [];
+  lightsColors = [];
+
+  for (let i = 0; i < 3; i++) {
+    colors[i] = createColorPicker("#FFFFFF");
+    colors[i].position(20,120+(i*45))
+    colors[i].input(() => {
+    let color = colors[i].color();
+
+    lightsColors[i*4+0] = red(color) / 255;
+    lightsColors[i*4+1] = green(color) / 255;
+    lightsColors[i*4+2] = blue(color) / 255;
+    lightsColors[i*4+3] = 1;
+  
+    myShader.setUniform("lightsColors", lightsColors);
+    });
+  }
+
   
   colorPicker = createColorPicker("#FFFFFF");
   colorPicker.position(400,80)
   colorPicker.input(() => {
   let color = colorPicker.color();
 
-  myShader.setUniform("lightColor", [
+  myShader.setUniform("ambientColor", [
       red(color) / 255,
       green(color) / 255,
       blue(color) / 255,
@@ -79,20 +103,25 @@ function setup() {
   phongExpSlider.position(350, 470);
   phongExpSlider.input(() => { myShader.setUniform('phongExp', phongExpSlider.value()) });
 
-  attenuationRate = createSlider(0, 0.1, 0.05, 0.001);
+  attenuationRate = createSlider(0, 0.1, 0.03, 0.001);
   attenuationRate.position(10, 470);
   attenuationRate.input(() => { myShader.setUniform('attenuationRate', attenuationRate.value()) });
+
+  lights = [];
   
   shader(myShader);
   myShader.setUniform('ambient', ambientSlider.value());
-  myShader.setUniform("lightColor", [1, 1, 1, 1]);
+  myShader.setUniform("ambientColor", [1, 1, 1, 1]);
   myShader.setUniform('phongExp', phongExpSlider.value());
   myShader.setUniform('attenuationRate', attenuationRate.value());
+  myShader.setUniform('lightCount', 1);
+  myShader.setUniform('lightsColors', [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
 }
 
 function draw() {
   
-  background(0);
+  
+  background(0);  
   let point = updatePointLight();
   resetShader();
   
@@ -100,17 +129,21 @@ function draw() {
   stroke('blue');
   axes();
   grid();
-  pop();
+  pop(); 
+
   
-  push();
-  translate(point.position);
-  noStroke();
-  fill('white');
-  sphere(3);
-  pop();
-  
+  for (let i = 0; i < selectPos.value(); i++) {    
+    push();
+    translate(point[i].position);
+    noStroke();
+    fill(colors[i].color());
+    sphere(3);
+    pop();
+    lights[i] = treeLocation(point[i].position, { from: Tree.WORLD, to: Tree.EYE }).array();
+  }
+
   shader(myShader);
-  myShader.setUniform('uLightPosition', treeLocation(point.position, { from: Tree.WORLD, to: Tree.EYE }).array());
+  myShader.setUniform('uLightPosition', lights.flat());  
   
   for (let i = 0; i < itemsPlane.value(); i++) {
     push();
@@ -132,11 +165,17 @@ function updatePointLight() {
   let g = (sin(mov * 0.5 + PI ) * 0.5 + 0.5);
   let b = (sin(frameCount * 0.05) * 0.5 + 0.5);
   let pz = sin(frameCount * 0.05);
-  return {
-    position: selectPos.value() === 'Centro' ? createVector(px, py, 0) : selectPos.value() === 'Izquierda' ?
-      createVector(50, 50, pz * 70) : createVector(-50, -50, -pz * 70),
-    color: selectPos.value() === 'Centro' ? color(1 - r, r / 2, r) : selectPos.value() === 'Izquierda' ?
-      color(r, 1, g) : color(1, r, g)
-  };
+  return [{
+    position: createVector(px, py, 0),
+    color: color(1 - r, r / 2, r)
+  },
+  {
+    position: createVector(50, 50, pz * 70),
+    color: color(r, 1, g)
+  },
+  {
+    position: createVector(-50, -50, -pz * 70),
+    color: color(1, r, g)
+  }];
 }
 
